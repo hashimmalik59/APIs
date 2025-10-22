8a45be92e263e5a2edefe153bd2eb13c130eca07
hashimmalik59 <hm3630450@gmail.com>
2025-10-22 23:57:45 +0500

    Day2 of learning APIs

---
+++ Day2/02_cookie.js
@@ -0,0 +1,7 @@
+// Cookie
+
+// Cookie aik small text file hota hai jo browser mein store hota hai
+// 1. size 4kb
+// 2. expire hosakta hai
+// 3. automatically data send karta hai
+// 4. iska kaam theme, preferences, login session
